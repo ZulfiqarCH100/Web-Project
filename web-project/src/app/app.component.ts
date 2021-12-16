e76a9e2ccae4741d6c8d3da54d658ad416c5ea48
Haissam Tariq Zaman <haissam.tariq@gmail.com>
2021-12-16 20:16:46 +0500

    added analytics

--- web-project/src/app/app.component.ts
+++ web-project/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var gtag: any;
 
 //Runs first.
 @Component({
@@ -6,4 +10,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html', //Renders this page.
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(router: Router) {
+    const navEndEvents = router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    );
+    navEndEvents.subscribe((event: any) => {
+      gtag('config', 'G-WEHB3PHSMJ', {
+        page_path: event.urlAfterRedirects,
+      });
+    });
+  }
+}
